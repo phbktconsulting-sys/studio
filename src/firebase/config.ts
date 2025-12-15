5c426309b5f3a79b49026393e8830deff81cfacd
phbkt <phbktconsulting@gmail.com>
2025-12-15 16:26:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8081664490-573e2",
+  "appId": "1:414864878828:web:8e57440f27acb20d5889c9",
+  "apiKey": "AIzaSyBMZzngxVVG8hVyup5z7T72piCQuU8POWU",
+  "authDomain": "studio-8081664490-573e2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "414864878828"
+};
