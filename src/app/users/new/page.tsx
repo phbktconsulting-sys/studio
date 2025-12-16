30cc338c0ade35eebdadc5b09a758cfbd384245f
phbkt <phbktconsulting@gmail.com>
2025-12-16 06:58:06 +0000

    create new user page all text font style change protectional

--- src/app/users/new/page.tsx
+++ src/app/users/new/page.tsx
@@ -117,7 +117,7 @@ export default function NewUserPage() {
   };
 
   return (
-    <div className="p-4 sm:p-6 font-mono">
+    <div className="p-4 sm:p-6">
        <div className="flex items-center gap-4 mb-6">
         <Button variant="outline" size="icon" onClick={() => router.back()}>
           <ArrowLeft className="h-4 w-4" />
