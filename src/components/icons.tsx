5dbc5d6f109c73922bbf90d9f05e91164cfa9b08
phbkt <phbktconsulting@gmail.com>
2025-12-25 09:10:41 +0000

    company logo not show correct

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,15 +1,13 @@
 import type { SVGProps } from 'react';
-import Image from 'next/image';
 
 export const LogoIcon = ({ className, ...props }: SVGProps<SVGSVGElement> & {height?: number, width?: number}) => {
   return (
-    <Image
+    <img
       src="/company-logo.png"
       alt="Company Logo"
-      width={props.width || 40}
       height={props.height || 40}
+      width={props.width || 40}
       className={className}
-      priority 
     />
   );
 };
