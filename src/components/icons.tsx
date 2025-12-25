c3eb3428955dd94ef017472f422bea602ce3d765
phbkt <phbktconsulting@gmail.com>
2025-12-25 08:55:39 +0000

    add farmer symbol best

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -8,24 +8,24 @@ export const LogoIcon = ({ className, ...props }: SVGProps<SVGSVGElement>) => {
       className={className}
       {...props}
     >
-      <g stroke="hsl(var(--foreground))" strokeWidth="4" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        {/* Mane */}
-        <path d="M 50,15
-                 L 60,25 75,25 80,35 85,50 80,65 75,75 60,75
-                 L 50,85
-                 L 40,75 25,75 20,65 15,50 20,35 25,25 40,25 Z" />
-        {/* Face Outline */}
-        <path d="M 50,30
-                 C 40,30 35,40 35,50
-                 C 35,65 40,70 50,70
-                 C 60,70 65,65 65,50
-                 C 65,40 60,30 50,30 Z" />
-        {/* Eyes */}
-        <circle cx="43" cy="48" r="2" fill="hsl(var(--foreground))" />
-        <circle cx="57" cy="48" r="2" fill="hsl(var(--foreground))" />
-        {/* Nose and Mouth */}
-        <path d="M 50,55 L 50,65" />
-        <path d="M 45,65 L 55,65" />
+      <g fill="hsl(var(--foreground))">
+        {/* Farmer's Hat */}
+        <path d="M 25,45
+                 A 30 10 0 0 0 75,45
+                 L 70,35
+                 A 20 20 0 0 0 30,35 Z" />
+
+        {/* Farmer's Head and Body */}
+        <circle cx="50" cy="55" r="10" />
+        <path d="M 35,65
+                 L 35,85
+                 L 65,85
+                 L 65,65
+                 A 15 15 0 0 0 35,65 Z" />
+
+        {/* Plant/Sprout in Hand */}
+        <path stroke="hsl(var(--background))" strokeWidth="3" d="M 60 75 C 65 70, 75 65, 80 55" />
+        <path fill="hsl(var(--background))" d="M 80 55 C 75 60, 85 60, 80 55" />
       </g>
     </svg>
   );
