5a94d40eb01dea7c7012521c55ee8a4e3fd8b3a7
phbkt <phbktconsulting@gmail.com>
2025-12-25 08:05:58 +0000

    can you add best logo very creative

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -11,17 +11,16 @@ export const LogoIcon = ({ className, ...props }: SVGProps<SVGSVGElement>) => {
     >
       <g transform="translate(50 50) scale(0.8)">
         <g transform="translate(-50 -50)">
+          {/* Background circle */}
+          <circle cx="50" cy="50" r="50" fill="hsl(var(--primary))" />
+          {/* Stylized 'W' for Workflow */}
           <path
-            d="M50,5A45,45,0,1,1,5,50,45,45,0,0,1,50,5m0-5A50,50,0,1,0,100,50,50,50,0,0,0,50,0Z"
-            fill="hsl(var(--primary))"
-          />
-          <path
-            d="M55.2,64.18,39.69,50,55.2,35.82,50,31,31,50,50,69.18Z"
-            fill="hsl(var(--primary))"
-          />
-          <path
-            d="M69,50,50,31,44.8,35.82,60.31,50,44.8,64.18,50,69.18Z"
-            fill="hsl(var(--primary))"
+            d="M25 35 L40 65 L50 45 L60 65 L75 35"
+            stroke="hsl(var(--primary-foreground))"
+            strokeWidth="8"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </g>
       </g>
