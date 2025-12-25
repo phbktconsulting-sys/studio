fe7acae547261b097e8ce0f9dc042d26aeecb165
phbkt <phbktconsulting@gmail.com>
2025-12-25 09:01:33 +0000

    the logo is diffrent and best

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -8,19 +8,14 @@ export const LogoIcon = ({ className, ...props }: SVGProps<SVGSVGElement>) => {
       className={className}
       {...props}
     >
-      <g fill="hsl(var(--foreground))">
-        {/* Stylized Phoenix */}
-        <path d="M 50,20 
-                 C 70,30 80,50 80,60 
-                 C 80,80 65,90 50,85 
-                 C 35,90 20,80 20,60 
-                 C 20,50 30,30 50,20 Z" fill="none" stroke="hsl(var(--foreground))" strokeWidth="5"/>
-        <path d="M 50,45
-                 C 60,50 65,60 65,70" fill="none" stroke="hsl(var(--foreground))" strokeWidth="5" />
-        <path d="M 50,45
-                 C 40,50 35,60 35,70" fill="none" stroke="hsl(var(--foreground))" strokeWidth="5" />
-        <path d="M 40,25 
-                 A 10 10 0 0 1 60,25" fill="none" stroke="hsl(var(--foreground))" strokeWidth="5" />
+      <g fill="none" stroke="hsl(var(--foreground))" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round">
+        {/* The 'P' shape */}
+        <path d="M 30,75 V 25" />
+        <path d="M 30,25 
+                 C 30,25 65,25 65,50 
+                 S 30,75 30,75" />
+        {/* The Arrow */}
+        <path d="M 55,40 L 75,50 L 55,60" />
       </g>
     </svg>
   );
