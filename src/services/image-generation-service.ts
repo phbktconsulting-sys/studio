2a18e4ca14d1feb1dd6b2f2a2e40ec8090a5a273
phbkt <phbktconsulting@gmail.com>
2025-12-27 05:29:44 +0000

    this error

--- src/services/image-generation-service.ts
+++ src/services/image-generation-service.ts
@@ -9,9 +9,7 @@ export async function generatePdfFromHtml(htmlContent: string): Promise<string> 
   try {
     browser = await puppeteer.launch({
       args: [...chromium.args, '--disable-web-security'],
-      executablePath: await chromium.executablePath(
-        `https://github.com/Sparticuz/chromium/releases/download/v119.0.0/chromium-v119.0.0-pack.tar`
-      ),
+      executablePath: await chromium.executablePath(),
       headless: chromium.headless,
     });
     
