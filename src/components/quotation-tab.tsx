271dc2c64abc2f896879bcb690fdc7790634e036
phbkt <phbktconsulting@gmail.com>
2025-12-28 06:35:41 +0000

    move more

--- src/components/quotation-tab.tsx
+++ src/components/quotation-tab.tsx
@@ -217,7 +217,7 @@ export function QuotationTab({ workItem }: QuotationTabProps) {
   }
   
   return (
-      <div className="p-2 space-y-6">
+      <div className="space-y-6">
         <div className="flex items-center gap-4">
             <Button variant="ghost" size="icon" onClick={() => setIsCqPageVisible(false)}>
                 <ArrowLeft className="h-4 w-4" />
