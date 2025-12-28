d76517ab8327d845e38d07edce994a2feaab8932
phbkt <phbktconsulting@gmail.com>
2025-12-28 10:05:05 +0000

    WorkFlow Management Application
    Hari Garad - Home Page text colour litt

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -58,10 +58,10 @@ export function AppHeader() {
           </div>
 
           <div className="ml-10 flex-1 space-y-1">
-            <p className="text-lg font-bold text-accent">
+            <p className="text-lg font-bold text-primary">
               WorkFlow Management Application
             </p>
-            <p className="text-base text-accent">
+            <p className="text-base text-primary">
               {user?.firstName} {user?.lastName} - Home Page
             </p>
           </div>
