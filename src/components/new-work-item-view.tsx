b191453dc41c8e602bc46762fd393c3082f0d9f8
phbkt <phbktconsulting@gmail.com>
2025-12-28 10:09:56 +0000

    Try fixing this error: `Runtime ReferenceError: RadioGroup is not define

--- src/components/new-work-item-view.tsx
+++ src/components/new-work-item-view.tsx
@@ -45,6 +45,7 @@ import {
 } from '@/components/ui/command';
 import { cn } from '@/lib/utils';
 import { Badge } from './ui/badge';
+import { RadioGroup, RadioGroupItem } from './ui/radio-group';
 
 const processTaskMap: Record<string, string[]> = {
     "New Business Request": ["Request Inmation & Quotation", "Request Website Development", "Request Mobile App Development", "Request Digital Marketing", "Request Meeting/Consultation", "Request Backend Support", "Request Graphic Design", "Request SEO Services", "Request Product Demo", "Request Project Proposal", "Request Maintenance Contract (AMC)", "Request Domain & Hosting", "Request Content Writing", "Request E-commerce Solution", "Request Automation & Micros", "Request Custom Software", "Request Urgent Repair (New Client)", "Request Callback", "Request Call for New Lead", "Request Other Services"],
