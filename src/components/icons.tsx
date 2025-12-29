0397e7ed5cc624f379f1e258561ce948e4c4fca5
phbkt <phbktconsulting@gmail.com>
2025-12-29 06:02:54 +0000

    the logo all colour same width

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,12 +12,12 @@ export const LogoIcon = ({ className, ...props }: SVGProps<SVGSVGElement> & {hei
       {...props}
     >
       <g transform="translate(50,50)">
-        <path d="M0,0 L-50,0 A50,50 0 0,1 0,-50 Z" fill="hsl(var(--chart-1))" transform="rotate(0)" />
-        <path d="M0,0 L-50,0 A50,50 0 0,1 0,-50 Z" fill="hsl(var(--chart-2))" transform="rotate(60)" />
-        <path d="M0,0 L-50,0 A50,50 0 0,1 0,-50 Z" fill="hsl(var(--chart-3))" transform="rotate(120)" />
-        <path d="M0,0 L-50,0 A50,50 0 0,1 0,-50 Z" fill="hsl(var(--chart-4))" transform="rotate(180)" />
-        <path d="M0,0 L-50,0 A50,50 0 0,1 0,-50 Z" fill="hsl(var(--primary))" transform="rotate(240)" />
-        <path d="M0,0 L-50,0 A50,50 0 0,1 0,-50 Z" fill="hsl(var(--accent))" transform="rotate(300)" />
+        <path d="M0,0 L50,0 A50,50 0 0,0 25,-43.3 Z" fill="hsl(var(--chart-1))" transform="rotate(0)" />
+        <path d="M0,0 L50,0 A50,50 0 0,0 25,-43.3 Z" fill="hsl(var(--chart-2))" transform="rotate(60)" />
+        <path d="M0,0 L50,0 A50,50 0 0,0 25,-43.3 Z" fill="hsl(var(--chart-3))" transform="rotate(120)" />
+        <path d="M0,0 L50,0 A50,50 0 0,0 25,-43.3 Z" fill="hsl(var(--chart-4))" transform="rotate(180)" />
+        <path d="M0,0 L50,0 A50,50 0 0,0 25,-43.3 Z" fill="hsl(var(--primary))" transform="rotate(240)" />
+        <path d="M0,0 L50,0 A50,50 0 0,0 25,-43.3 Z" fill="hsl(var(--accent))" transform="rotate(300)" />
       </g>
     </svg>
   );
